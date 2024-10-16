f690130a1ce48350a2f001dd0049655a801e517c
Rafael Soares Sobucki <rafaelsobucki.smartsoftware@bexsbanco.com.br>
2024-10-16 17:59:46 -0300

    Configuration of typeorm

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "1234",
+  database: "memory-local",
+  synchronize: false,
+  logging: true,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+});
